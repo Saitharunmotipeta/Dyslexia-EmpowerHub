80e36000a6fb6001e850a2ed4708881312a5fe37
Saitharunmotipeta04 <saitharun.m@exterview.ai>
2026-01-09 01:13:45 +0530

    revised mocktest module with report generation

--- backend/app/mock/routes/__init__.py
+++ backend/app/mock/routes/__init__.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from app.mock.routes.start import start_mock
 from app.mock.routes.word import submit_mock_word
 from app.mock.routes.result import get_mock_result
+from app.mock.routes.report import download_mock_report_handler
 
 router = APIRouter(prefix="/mock", tags=["Mock Test"])
 
 router.post("/start")(start_mock)
 router.post("/word")(submit_mock_word)
 router.post("/result")(get_mock_result)
+router.get("/report", response_model=None)(download_mock_report_handler)
