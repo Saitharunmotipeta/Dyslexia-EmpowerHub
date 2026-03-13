04f34be618dc198c94b164d8b1aa65378d0aaf61
Saitharunmotipeta <motipetasaitharun@gmail.com>
2026-03-13 12:25:10 +0530

    reinitialised the frontend origin

--- Frontend/lib/api.ts
+++ Frontend/lib/api.ts
@@ -4,7 +4,7 @@
  */
 
 const getBaseUrl = () => {
-  const url = "http://localhost:8000"; // process.env.NEXT_PUBLIC_API_BASE_URL;
+  const url = process.env.NEXT_PUBLIC_API_BASE_URL ||  "http://localhost:8000";
   if (!url) throw new Error("NEXT_PUBLIC_API_BASE_URL is not set");
   return url.replace(/\/$/, "");
 };
